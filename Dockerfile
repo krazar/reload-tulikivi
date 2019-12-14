169e8656fd5f98011055b961562881792d1ed839
Pierre Marot <pierre@qualifio.com>
2019-12-14 18:44:26 +0100

    add mqtt connection

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
 FROM mohaseeb/raspberrypi3-python-opencv:4.1.0
-ADD . /
 RUN pip install paho-mqtt
-
+ADD . /
 CMD ["python", "/tulikivi.py"]
--- tulikivi.py
+++ tulikivi.py
@@ -1,8 +1,16 @@
 import cv2
 import numpy as np
 import os
+import paho.mqtt.client as mqttClient
 import time
 
+def on_connect(client, userdata, flags, rc):
+  if rc == 0:
+    print("Connected to broker")
+    global Connected                #Use global variable
+    Connected = True                #Signal connection
+  else:
+    print("Connection failed")
 
 def readTulikiviLight(frame):
   lower = np.array([0, 120, 150])
@@ -30,17 +38,38 @@ def analyseImage(cap):
     ret = readTulikiviLight(image)
     return ret
 
-def process(cap):
+def process(cap, mqttClient):
   result = analyseImage(cap)
-  print(result)
+  mqttClient.publish("tulikivi/state", result)
+
+def mqttConnection(url, port, user, password):
+  client = mqttClient.Client("Python")               #create new instance
+  client.username_pw_set(user, password=password)    #set username and password
+  client.on_connect= on_connect                      #attach function to callback
+  client.connect(url, port=port)          #connect to broker
+
+  client.loop_start()        #start the loop
+
+  while Connected != True:    #Wait for connection
+    time.sleep(0.1)
+  return client;
+
 
 
 def main():
+  broker = os.environ['MQTT_URL']
+  port = os.environ['MQTT_PORT']
+  user = os.environ['MQTT_USER']
+  password = os.environ['MQTT_PASS']
   url = os.environ['CAMERA_URL']
+  client = mqttConnection(broker, port, user, password)
   cap = cv2.VideoCapture(url)
-  process(cap)
-  time.sleep(5)
+  process(cap, client)
   closeImage(cap)
+  client.disconnect()
+  client.loop_stop()
+
+Connected = False   #global variable for the state of the connection
 
 main()
 
