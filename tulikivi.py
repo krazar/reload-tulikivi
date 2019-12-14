dc849ed8af93ba938263959e3ce4264ed98d8a71
Pierre Marot <pierre@qualifio.com>
2019-12-14 17:53:49 +0100

    small refactor

--- tulikivi.py
+++ tulikivi.py
@@ -1,32 +1,49 @@
 import cv2
 import numpy as np
-import os 
+import os
+import time
 
-url = os.environ['CAMERA_URL']
-cap = cv2.VideoCapture(url);
-ret, frame = cap.read()
-
-
-if (ret == True):
-
-  frame = frame[250:295, 445:491]
+starttime=time.time()
 
 
-  lower = np.array([210, 183, 209])
+def readTulikiviLight(frame):
+  lower = np.array([0, 120, 150])
   upper = np.array([255, 255, 255])
 
   mask = cv2.inRange(frame, lower, upper)
   nonZero = cv2.countNonZero(mask)
-
   height, width = mask.shape
   size = height * width
+  ret = 0
+  if (nonZero != 0):
+    ret = nonZero * 100 / size
+  return ret
 
-  if (nonZero == 0):
-    print(0)
-  else:
-    print(nonZero / size)
+def cropImage(frame):
+  return frame[250:295, 445:491]
 
+def closeImage(cap):
   cap.release()
 
+def analyseImage(cap):
+  ret, frame = cap.read()
+  if (ret == True):
+    image = cropImage(frame)
+    ret = readTulikiviLight(image)
+    return ret
+
+def process(cap):
+  result = analyseImage(cap)
+  print(result)
+
+
+def main():
+  url = os.environ['CAMERA_URL']
+  cap = cv2.VideoCapture(url)
+  process(cap)
+  time.sleep(5)
+  closeImage(cap)
+
+main()
 
 
